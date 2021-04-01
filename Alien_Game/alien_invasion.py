ca3de0c2c512590e657c198a209ad5353ed57c38
Paul <pawelpelar@gmail.com>
2021-04-01 17:03:31 +0200

    Bullets finished

--- Alien_Game/alien_invasion.py
+++ Alien_Game/alien_invasion.py
@@ -32,7 +32,7 @@ class AlienInvasion:
             #Oczekiwanie na nacisniecie klawisza badz przycisku myszy
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
 
     def _check_events(self):
@@ -74,8 +74,17 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         '''Stworzenie pocisku i dodanie go do grupe pocisków'''
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        '''Aktualizacja pocisków i usuwanie tych spoza ekranu'''
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         '''Uaktualnienie ekranu'''
--- Alien_Game/settings.py
+++ Alien_Game/settings.py
@@ -20,3 +20,4 @@ class Settings:
         self.bullet_width = 5
         self.bullet_height = 20
         self.bullet_color = (255,0,0)
+        self.bullets_allowed = 3
